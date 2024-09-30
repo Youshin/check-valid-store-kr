dfbe4be70204cfb71bd820d9d875a6e58b6d0c12
Youshin Kim <youshin1411@gmail.com>
2024-09-30 12:40:03 +0900

    extract yaml

--- extract_yaml.py
+++ extract_yaml.py
@@ -4,7 +4,5 @@ with open("location.yaml", "r") as file:
     data = yaml.safe_load(file)
 for location in data["locations"]:
     for store_name, details in location.items():
-        print(store_name)
-        print(details)
         address = details[0]["주소"]
         print(f"{store_name},{address}")
